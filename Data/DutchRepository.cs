34af7a760acbe4b8a395f8298b887f7c944b2db7
Nathan Costea <cnlnat@yahoo.com>
2018-10-24 16:14:25 -0400

    Adding a Repository with 2 functions so far.

--- Data/DutchRepository.cs
+++ Data/DutchRepository.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DutchTreat.Data.Entities;
+
 namespace DutchTreat.Data
 {
     public class DutchRepository
     {
+        private readonly DutchContext _ctx;
+
         public DutchRepository(DutchContext ctx)
         {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _ctx.Products
+                       .OrderBy(o => o.Title)
+                       .ToList();
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return _ctx.Products
+                       .Where(w => w.Title == category)
+                       .OrderBy(o => o.Title)
+                       .ToList();
         }
     }
 }
